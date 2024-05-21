88cfde4df74c67b03431549785fd779572a225c5
ECegal <eder@lasertechbrasil.com.br>
2024-05-21 16:12:33 -0300

    corrigindo cagada do main

---
+++ TesteVideoDevice/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TesteVideoDevice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxSaida = new System.Windows.Forms.TextBox();
+            this.pctImage = new System.Windows.Forms.PictureBox();
+            this.btnGetImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxSaida
+            // 
+            this.tbxSaida.Location = new System.Drawing.Point(12, 66);
+            this.tbxSaida.Multiline = true;
+            this.tbxSaida.Name = "tbxSaida";
+            this.tbxSaida.Size = new System.Drawing.Size(417, 289);
+            this.tbxSaida.TabIndex = 0;
+            // 
+            // pctImage
+            // 
+            this.pctImage.Location = new System.Drawing.Point(435, 66);
+            this.pctImage.Name = "pctImage";
+            this.pctImage.Size = new System.Drawing.Size(353, 289);
+            this.pctImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctImage.TabIndex = 1;
+            this.pctImage.TabStop = false;
+            // 
+            // btnGetImage
+            // 
+            this.btnGetImage.Location = new System.Drawing.Point(12, 25);
+            this.btnGetImage.Name = "btnGetImage";
+            this.btnGetImage.Size = new System.Drawing.Size(75, 23);
+            this.btnGetImage.TabIndex = 2;
+            this.btnGetImage.Text = "Disparar";
+            this.btnGetImage.UseVisualStyleBackColor = true;
+            this.btnGetImage.Click += new System.EventHandler(this.btnGetImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 365);
+            this.Controls.Add(this.btnGetImage);
+            this.Controls.Add(this.pctImage);
+            this.Controls.Add(this.tbxSaida);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxSaida;
+        private System.Windows.Forms.PictureBox pctImage;
+        private System.Windows.Forms.Button btnGetImage;
+    }
+}
+
---
+++ TesteVideoDevice/Form1.cs
@@ -0,0 +1,95 @@
+﻿using br.com.ltb.Camera.Pumatronix;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteVideoDevice
+{
+    public partial class Form1 : Form
+    {
+
+        private Pumatronix _camera;
+        private int imageNumber = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _camera = new Pumatronix("192.168.50.172");
+            _camera.Open();
+        }
+
+        private void btnGetImage_Click(object sender, EventArgs e)
+        {
+            var desired = new List<ImageField.TypeOfField>() { ImageField.TypeOfField.Horario };
+            var img = _camera.GetImage(80, desired);
+            var now = DateTime.Now;
+            TriggerInfo inf = new TriggerInfo()
+            {
+                CameraDateTime = img.SortedFields[ImageField.TypeOfField.Horario],
+                SystemDateTime = now.ToString("dd/MM/yyyy HH:mm:ss"),
+                ImageName = now.ToString("yyyyMMddHHmmss") + "-" + imageNumber
+            };
+
+            imageNumber++;
+
+            var format = new JsonSerializerSettings();
+            format.Formatting = Formatting.Indented;
+            string serialized = JsonConvert.SerializeObject(inf,format);
+            tbxSaida.Text = serialized;
+
+            pctImage.Image = img.Image;
+            sendJson(serialized);
+        }
+
+        private void sendJson(string message)
+        {
+            Uri address;
+            address = new Uri("http://127.0.0.1:51000");
+           
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = address;
+                    client.Timeout = new TimeSpan(0, 0, 5000);
+                    var content = new StringContent(message);
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                    var response = client.PostAsync(string.Empty, content).Result;
+
+
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {                       
+                        throw new Exception($"Fail to send, status code: {response.StatusCode}");
+                    }
+                    else
+                    {
+                       
+                    }
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+              
+                throw;
+            }
+        }
+    }
+
+    public class TriggerInfo
+    {
+        public string CameraDateTime { get; set; }
+        public string SystemDateTime { get; set; }
+        public string ImageName { get; set; } 
+    }
+}
