945e401b5e5fa5be95db4839aeed6002f4a3a28d
KiraVM <vinicius.akira@lasertechbrasil.com.br>
2024-05-22 20:21:12 -0300

    Correção do teste e add de validações

--- TesteVideoDevice/Form1.Designer.cs
+++ TesteVideoDevice/Form1.Designer.cs
@@ -31,6 +31,12 @@
             this.tbxSaida = new System.Windows.Forms.TextBox();
             this.pctImage = new System.Windows.Forms.PictureBox();
             this.btnGetImage = new System.Windows.Forms.Button();
+            this.tbxIpCamera = new System.Windows.Forms.TextBox();
+            this.tbxIpServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblStatusCamera = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pctImage)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,7 +59,8 @@
             // 
             // btnGetImage
             // 
-            this.btnGetImage.Location = new System.Drawing.Point(12, 25);
+            this.btnGetImage.Enabled = false;
+            this.btnGetImage.Location = new System.Drawing.Point(713, 21);
             this.btnGetImage.Name = "btnGetImage";
             this.btnGetImage.Size = new System.Drawing.Size(75, 23);
             this.btnGetImage.TabIndex = 2;
@@ -61,16 +68,74 @@
             this.btnGetImage.UseVisualStyleBackColor = true;
             this.btnGetImage.Click += new System.EventHandler(this.btnGetImage_Click);
             // 
+            // tbxIpCamera
+            // 
+            this.tbxIpCamera.Location = new System.Drawing.Point(92, 6);
+            this.tbxIpCamera.Name = "tbxIpCamera";
+            this.tbxIpCamera.Size = new System.Drawing.Size(140, 20);
+            this.tbxIpCamera.TabIndex = 3;
+            this.tbxIpCamera.Text = "192.168.50.172";
+            // 
+            // tbxIpServer
+            // 
+            this.tbxIpServer.Location = new System.Drawing.Point(92, 32);
+            this.tbxIpServer.Name = "tbxIpServer";
+            this.tbxIpServer.Size = new System.Drawing.Size(140, 20);
+            this.tbxIpServer.TabIndex = 4;
+            this.tbxIpServer.Text = "127.0.0.1:51000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP da Câmera:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "IP do Servidor:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(238, 6);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblStatusCamera
+            // 
+            this.lblStatusCamera.Location = new System.Drawing.Point(319, 9);
+            this.lblStatusCamera.Name = "lblStatusCamera";
+            this.lblStatusCamera.Size = new System.Drawing.Size(96, 13);
+            this.lblStatusCamera.TabIndex = 8;
+            this.lblStatusCamera.Text = "Desconectada";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 365);
+            this.Controls.Add(this.lblStatusCamera);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxIpServer);
+            this.Controls.Add(this.tbxIpCamera);
             this.Controls.Add(this.btnGetImage);
             this.Controls.Add(this.pctImage);
             this.Controls.Add(this.tbxSaida);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Trigger";
             ((System.ComponentModel.ISupportInitialize)(this.pctImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -82,6 +147,12 @@
         private System.Windows.Forms.TextBox tbxSaida;
         private System.Windows.Forms.PictureBox pctImage;
         private System.Windows.Forms.Button btnGetImage;
+        private System.Windows.Forms.TextBox tbxIpCamera;
+        private System.Windows.Forms.TextBox tbxIpServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblStatusCamera;
     }
 }
 
--- TesteVideoDevice/Form1.cs
+++ TesteVideoDevice/Form1.cs
@@ -11,6 +11,8 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static br.com.ltb.Camera.Pumatronix.Pumatronix;
+using System.Threading;
 
 namespace TesteVideoDevice
 {
@@ -19,52 +21,74 @@ namespace TesteVideoDevice
 
         private Pumatronix _camera;
         private int imageNumber = 1;
+        private Status _statusCamera = Status.Desconectada;
 
         public Form1()
         {
-            InitializeComponent();
-            _camera = new Pumatronix("192.168.50.172");
-            _camera.Open();
+            InitializeComponent();            
         }
 
         private void btnGetImage_Click(object sender, EventArgs e)
         {
             var desired = new List<ImageField.TypeOfField>() { ImageField.TypeOfField.Horario };
-            var img = _camera.GetImage(80, desired);
-            var now = DateTime.Now;
-            TriggerInfo inf = new TriggerInfo()
+
+            try
             {
-                CameraDateTime = img.SortedFields[ImageField.TypeOfField.Horario],
-                SystemDateTime = now.ToString("dd/MM/yyyy HH:mm:ss"),
-                ImageName = now.ToString("yyyyMMddHHmmss") + "-" + imageNumber
-            };
+                var img = _camera.GetImage(80, desired);
+                var now = DateTime.Now;
 
-            imageNumber++;
+                TriggerInfo inf = new TriggerInfo()
+                {
+                    CameraDateTime = img.SortedFields[ImageField.TypeOfField.Horario],
+                    SystemDateTime = now.ToString("dd/MM/yyyy HH:mm:ss"),
+                    ImageName = now.ToString("yyyyMMddHHmmss") + "-" + imageNumber
+                };
 
-            var format = new JsonSerializerSettings();
-            format.Formatting = Formatting.Indented;
-            string serialized = JsonConvert.SerializeObject(inf,format);
-            tbxSaida.Text = serialized;
+                imageNumber++;
 
-            pctImage.Image = img.Image;
-            sendJson(serialized);
+                var format = new JsonSerializerSettings();
+                format.Formatting = Formatting.Indented;
+                string serialized = JsonConvert.SerializeObject(inf, format);
+                tbxSaida.Text = serialized;
+
+                pctImage.Image = img.Image;
+
+                try
+                {
+                    sendJson(serialized);
+                }
+                catch (Exception ex2)
+                {
+                    throw new Exception("Erro a enviar o trigger para o servidor! Msg: " + ex2.InnerException.Message);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                if (IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        MessageBox.Show(this, ex.Message);
+                    });
+                }
+            }                        
         }
 
         private void sendJson(string message)
         {
             Uri address;
-            address = new Uri("http://127.0.0.1:51000");
-           
+            address = new Uri($"http://{tbxIpServer.Text}"); //127.0.0.1:51000
+
             try
             {
                 using (HttpClient client = new HttpClient())
                 {
                     client.BaseAddress = address;
-                    client.Timeout = new TimeSpan(0, 0, 5000);
+                    client.Timeout = new TimeSpan(0, 0, 4);
                     var content = new StringContent(message);
                     content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
                     var response = client.PostAsync(string.Empty, content).Result;
-
 
                     if (response.StatusCode != HttpStatusCode.OK)
                     {                       
@@ -72,17 +96,126 @@ namespace TesteVideoDevice
                     }
                     else
                     {
-                       
-                    }
-
-                    
+                        if(IsHandleCreated)
+                        {
+                            this.Invoke((MethodInvoker) delegate
+                            {
+                                tbxSaida.Text += $"\r\n{response.Content}";
+                            });
+                        }                        
+                    }                    
                 }
             }
             catch (Exception ex)
-            {
-              
-                throw;
+            {              
+                throw ex.InnerException;
             }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (_statusCamera == Status.Desconectada)
+            {
+                if (string.IsNullOrEmpty(tbxIpCamera.Text))
+                {
+                    if (IsHandleCreated)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            MessageBox.Show(this, "Digite o IP da Câmera!");
+                        });
+                    }
+
+                    return;
+                }
+
+                try
+                {
+                    _camera = new Pumatronix(tbxIpCamera.Text);
+                    _camera.CgiPort = 80;
+                    _camera.cameraconnection += _camera_cameraconnection;
+                    _camera.OpenAsync();
+                }
+                catch (Exception ex)
+                {
+                    _statusCamera = Status.Desconectada;
+                    _camera.cameraconnection -= _camera_cameraconnection;
+
+                    if (IsHandleCreated)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            MessageBox.Show(this, "Falha ao conectar com a câmera! Msg: " + ex.Message);
+                        });
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    _camera.Close();
+
+                    if (IsHandleCreated)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {                            
+                            btnGetImage.Enabled = false;
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        private void _camera_cameraconnection(object sender, Status connection_status)
+        {
+            _statusCamera = connection_status;
+
+            if (IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    lblStatusCamera.Text = connection_status.ToString();
+
+                    if (_statusCamera == Status.Conectada)
+                    {
+                        btnGetImage.Enabled = true;
+                        btnConnect.Text = "Desconectar";
+                        ThreadPool.QueueUserWorkItem(SendUrlDataHora);
+                    }
+                    else
+                    {
+                        btnGetImage.Enabled = false;
+                        btnConnect.Text = "Conectar";
+                    }
+                });
+            }            
+        }
+
+        private void SendUrlDataHora(object state)
+        {
+            try
+            {
+                string url = $"http://{tbxIpCamera.Text}/api/config.cgi?Data=" +
+                    $"{DateTime.Now.Day.ToString().PadLeft(2, '0')}{DateTime.Now.Month.ToString().PadLeft(2, '0')}{DateTime.Now.Year.ToString().Substring(2)}&" +
+                    $"Hora={DateTime.Now.Hour.ToString().PadLeft(2, '0')}{DateTime.Now.Minute.ToString().PadLeft(2, '0')}{DateTime.Now.Second.ToString().PadLeft(2, '0')}&" +
+                    $"HorarioVerao=0";
+
+                _camera.SendURL(url);
+            }
+            catch (Exception ex)
+            {
+                if(IsHandleCreated)
+                {
+                    MessageBox.Show(this, $"Falha ao altera a data/hora da câmera! MSG: {ex.Message}");
+                }
+            }
+            
         }
     }
 
